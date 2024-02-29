8a93aa06496c0a5074cd88762f29786b1381b1e8
Samarendra singh <118622849+Samar-project@users.noreply.github.com>
2024-02-29 16:17:59 +0530

    Update urls.py

--- tripService/trip/urls.py
+++ tripService/trip/urls.py
@@ -1,13 +1,13 @@
 # trip_app/urls.py
 
 from django.urls import path
-from .views import TripListCreateView, TripView,TripRetrieveUpdateDestroyView
+from .views import TripListCreateView,TripRetrieveUpdateDestroyView
 # from route.views import RouteListCreateView
 
 urlpatterns = [
-    path('list/', TripView.as_view(), name='trip'),
+    # path('list/', TripView.as_view(), name='trip'),
     # Add other URL patterns as needed
     path('lists/', TripListCreateView.as_view(), name='trip-list'),
-    path('details/', TripRetrieveUpdateDestroyView.as_view(), name='trip-details'),
+    path('lists/<pk>', TripRetrieveUpdateDestroyView.as_view(), name='trip-details'),
 
 ]
